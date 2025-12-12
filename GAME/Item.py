65961361cbaf61df44803108e7c96736a9a7f852
XuClaudia <6727@student.sgdb.nl>
2025-12-12 14:34:59 +0100

    Update Item.py

--- GAME/Item.py
+++ GAME/Item.py
@@ -10,33 +10,33 @@ class Item:
 import random
 
 class Weapon(Item):
-    def __init__(self, item_level):
+    def __init__(self, item_level, player_name):
         Item.__init__(self, item_level)
         
         self.item_type = "Weapon"
         
-###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        if player_type == "Finn" :
-            weapon_list = weapon_list_Finn = ["Pocket knife", "Sword", "Katana"]
-        elif player_type == "Ice_king":
-            weapon_list = weapon_list_Ice_king = ["Snow ball", "icicle", "Gunter"]
-        elif player_type == "Three_trunks" :
-            weapon_list = weapon_list_Three_trunks = ["Spoon", "Rock", "Apple pie"]
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        if player_name == "Finn" :
+            weapon_list = ["Pocket knife", "Sword", "Katana"]
+        elif player_name == "Ice king":
+            weapon_list = ["Snow ball", "icicle", "Gunter"]
+        elif player_name == "Three trunks" :
+            weapon_list = ["Spoon", "Rock", "Apple pie"]
 
 
 
         #self.weapon_type = random.choice(weapon_list)
-        self.weapon_type = weapon_list[1]
+        self.weapon_type = weapon_list[0]
         ### ik geef nu aan alle wapens op plek 1 dezelfde damage en plaats twee enz.
-        if self.weapon_type == weapon_list[1]:
+        if self.weapon_type == weapon_list[0]:
             self.min_damage = self.item_level * 2
             self.max_damage = self.item_level * 3
         
-        elif self.weapon_type == weapon_list[2]:
+        elif self.weapon_type == weapon_list[1]:
             self.min_damage= 3
             self.max_damage = self.item_level * 4
 
-        elif self.weapon_type == weapon_list[3]:
+        elif self.weapon_type == weapon_list[2]:
             self.min_damage = 5
             self.max_damage = self.item_level * 6
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -54,4 +54,5 @@ class Armor(Item):
         Item.print_stats(self)
         print("Defence:", self.defence)
         
+
 
