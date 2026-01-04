b2f41fb6bca58182499107514aba1323ffbf1498
XuClaudia <6727@student.sgdb.nl>
2026-01-04 22:03:29 +0100

    Update Game.py

--- GAME/Game.py
+++ GAME/Game.py
@@ -1,55 +1,66 @@
-#tekst letter voor letter laten verschijnen en niet in een pak
 import random
 import time
 from Player import Player
 from Item import Item, Weapon, Armor
 from Monster import Monster, Zombie, Candy, Vampire, Jelly, Evil_eyes, Goo_skulls
 from Battle import Battle
+import Ascii
 def text_effect(text):
     for letter in text:
         print(letter, end="")
         time.sleep(0.05)
-#welcome screen
-print("Hey! Hello there!")
+
+#startscherm
+starttekst = [
+    "Hey! Hello there!",
+    "",
+    "~~  Adventure Time Come on, grab your friends",
+    "~~  We'll go to very distant lands",
+    "~~  With Jake the Dog and Finn the Human",
+    "~~  The fun will never end, it's Adventure Time"
+    ]
+
+for tekst in starttekst:
+    print(tekst)
+    time.sleep(1.3)
+    
+
 input("[PRESS ENTER TO CONTINUE]")
-print()
-print("~~  Adventure Time Come on, grab your friends")
-time.sleep(1)
-print("~~  We'll go to very distant lands")
-time.sleep(1.5)
-print("~~  With Jake the Dog and Finn the Human")
-time.sleep(2)
-print("~~  The fun will never end, it's Adventure Time")
-time.sleep(1.5)
-print()
-input("[PRESS ENTER TO CONTINUE]")
-print()
+print("-"*75)
 
 #spelertype 
 player_name = " "
-while player_name not in ["A","B","C", "FINN", "ICE KING", "THREE TRUNKS"]:
-    player_name = input("Who do you want to be? (A): Ice king, (B): Finn, (C): Three trunks\n").upper()  #Speler kiest A, B of C 
+while player_name not in ["A","B","C", "FINN", "ICE KING", "TREE TRUNKS"]:
+    player_name = input("Who do you want to be? (A): Ice king, (B): Finn, (C): Tree trunks\n").upper()  #Speler kiest A, B of C 
     print()
 
-
-if player_name == "A" or player_name == "Ice king":
-    player = Player("Ice king") 
-elif player_name == "B" or player_name == "Finn":
-    player = Player("Finn") 
-elif player_name == "C" or player_name == "Three trunks":
-    player = Player("Three trunks") 
- 
 #player = Player(player_name)
+if player_name == "A" or player_name == "Ice king":
+    player = Player("Ice king")
+    Ascii.display_art("Ice king")
+elif player_name == "B" or player_name == "Finn":
+    player = Player("Finn")
+    Ascii.display_art("Finn")
+elif player_name == "C" or player_name == "Tree trunks":
+    player = Player("Tree trunks")
+    Ascii.display_art("Tree trunks")
+ 
+
     
-print()
 text_effect("Hi " + player.name + ", you are entering a world full of adventure....\n")
+#HIER MOET NOG TEKST BIJ VERZONNEN WORDEN
 for letter in "BE AWARE!!":
             print(letter, end=" ")
             time.sleep(0.25)
             
 print()         
 input("[PRESS ENTER TO CONTINUE]")
+print("-"*75)
 
+#HIER MOET SOWIESO NOG IETS OF EEN VERHAALTJE MAAR DA IS KWA CODE NI HEEL INTERESSANT
+#OF WE DOEN IETS VAN EEN MAP TOEVOEGEN?? DIE HIER NU KOMT
+#GW IETS ZODAT JE NIET BEGINT MET WOW GEVECHT
+#VISUEEL TEXT MAP???
 
 battle_count = 0
 
@@ -68,6 +79,8 @@ print("Your final stats are:")
 player.print_stats()
 print()
 print("Thanks for playing")
+
+
 
 
 
