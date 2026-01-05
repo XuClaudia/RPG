b5226ad042ea372843f211a5f45183aedc602a85
XuClaudia <6727@student.sgdb.nl>
2026-01-05 13:40:32 +0100

    Update Item.py

--- GAME/Item.py
+++ GAME/Item.py
@@ -1,101 +1,89 @@
-#let op toegevoegde gold_value
-import random, time
-class Monster:
-    hp = 1
-    max_hp = 1
-    min_damage = 1
-    max_damage = 1
-    monster_type = None
-    xp_value = 1
-    gold_value = 1
-    
-    def __init__(self, level):
-        self.level = level
+import time
+
+def text_effect(text):
+    for letter in text:
+        print(letter, end="")
+        time.sleep(0.01)
         
-    def attack(self):
-        damage = random.randint(self.min_damage, self.max_damage)
-        print(self.monster_type, "attacks for", damage, "damage")
-        return damage
+class Item:
+    inventory = []
+    item_type = None
     
-    def take_hit(self, damage):
-        self.hp -= damage
-        if self.hp > 0:
-            print(self.monster_type, "has", self.hp, "hp left")
-        else:
-            print(self.monster_type, "is slain")
-            
+    def __init__(self, item_level):
+        self.item_level = item_level
+        
     def print_stats(self):
-        print(self.monster_type, "- level", self.level)
-        if self.hp> 0:
-            print("HP:", self.hp, "/", self.max_hp)
-        else:
-            print(self.monster_type, "defeated")
-    #--------------------------------Verschillende soorten monsters -----------------------------------------
-# Spelertype 1: Ice king(Archer) Spelertype 2: Finn(sword player) Spelertype 3: Tree Trunks (archer)
-# Wapens voor type 1: a. Snowball b. Iceicle c. gunter(dingen gooien) Type 2: a. Pocket knife b. Finn's sword c. Katana Type: 3: a. spoon b. Rock c. Apple pie
-# Monsters die tegenkomt: A. Zombie B. Candy C. vampire D. Jelly Cube E. Evil Eyes F. Goo skulls
-
-class Zombie(Monster):                         #De zombie (monster) heeft als doel om de weapon damage van de speler te verlagen. 
-    def __init__(self, level):
-        Monster.__init__(self, level)
-        self.monster_type = "Zombie"
-        self.hp = self.max_hp = self.level * 10
-        #code is geplaatst naar Battle +- regel 111
-        #self.min_damage = self.weapon.min_damage - 1    # De weapon damage van de speler wordt met één verlaagd en zo dus ook verlaagd.
-        #self.max_damage = self.weapon.max_damage // 2    #De weapon damage van de speler wordt gehalveerd en zo dus verlaagd.
-        self.xp_value = 100 + self.level * 25
-        self.gold_value = 5 + self.level * 50
+        text_effect(self.item_type + " - level: " ) #+ str(self.item_level)
         
-class Candy(Monster):
-    def __init__(self, level):
-        Monster.__init__(self, level) 
-        self.monster_type = "Candy"
-        self.hp = self.max_hp = self.level * 5
-        self.min_damage = self.level + 2
-        self.max_damage = self.level + 4
-        self.xp_value = 100 + self.level * 25
-        self.gold_value = 5 + self.level * 50
+import random
+
+class Weapon(Item):
+    def __init__(self, item_level, player_name, weapon_name = None):
+        Item.__init__(self, item_level)
         
-class Vampire(Monster):                         
-    def __init__(self, level):
-        Monster.__init__(self, level)
-        self.monster_type = "Vampire"
-        self.hp = self.max_hp = self.level * 15
-        self.min_damage = self.level + 1
-        self.max_damage = self.level * 3
-        self.xp_value = 100 + self.level * 20
-        self.gold_value = 5 + self.level * 50
-
-class Jelly(Monster):
-    def __init__(self, level):
-        Monster.__init__(self, level)
-        self.monster_type = "Jelly"
-
-
-class Evil_eyes(Monster):
-    def __init__(self, level):
-        Monster.__init__(self, level)
-
-        self.monster_type = "Evil_eyes"
-
-
-class Goo_skulls(Monster):
-    def __init__(self, level):
-        Monster.__init__(self, level)   
-        self.monster_type = "Goo_skulls"
-        self.hp = self.max_hp = self.level * 20
-        self.min_damage = 1
-        self.max_damage = self.level * 4
-        self.xp_value = 100 + self.level * 20
-        self.gold_value = 5 + self.level * 50
-        self.crit_chance = max(30, level * 10)
+        self.item_type = "Weapon"
         
-    def attack(self):
-        damage = random.randint(self.min_damage, self.max_damage)
-        #crit hit?
-        if random.randint(1, 100) <= self.crit_chance:
-            print(self.monster_type, "makes a critical hit!")
-            damage *= 2
+        if weapon_name:
+            self.weapon_type = weapon_name #als de naam in de shop al was gegeven
+        elif player_name == "Finn" :
+            weapon_list = ["Pocket_knife", "Sword", "Katana"]
+        elif player_name == "Ice king":
+            weapon_list = ["Snowball", "icicle", "Gunter"]
+        elif player_name == "Tree trunks" :
+            weapon_list = ["Spoon", "Rock", "Applepie"]
+
+        self.weapon_list = weapon_list
+        self.weapon_type = weapon_list[0]
+        self.set_damage()
+       
+    def set_damage(self):
+        ### ik geef nu aan alle wapens op plek 1 dezelfde damage
+        if self.weapon_type == self.weapon_list[0]:
+            self.min_damage = self.item_level * 2
+            self.max_damage = self.item_level * 3
+        
+        elif self.weapon_type == self.weapon_list[1]:
+            self.min_damage=  self.item_level * 3
+            self.max_damage = self.item_level * 4
+
+        elif self.weapon_type == self.weapon_list[2]:
+            self.min_damage =  self.item_level * 4
+            self.max_damage = self.item_level * 5
             
-        print(self.monster_type, "attacks for", damage, "damage")
-        return damage
+        elif self.weapon_type == "Dragon_sword": #shop
+            self.min_damage =  self.item_level * 6
+            self.max_damage = self.item_level * 8
+            
+        elif self.weapon_type == "Fork": #shop
+            self.min_damage =  self.item_level * 5
+            self.max_damage = self.item_level * 8
+            
+    def upgrade_damage(self, upgrade_amount): #shop
+        self.min_damage += upgrade_amount
+        self.max_damage += upgrade_amount
+        print("⚡ Weapon upgraded! Damage: " + self.min_damage + " - " + self.max_damage)
+
+    def print_stats(self):
+        Item.print_stats(self)
+        print(self.weapon_type + "_damage: " + str(self.min_damage) + "-" + str(self.max_damage) + "\n")
+            
+class Armor(Item):
+    def __init__(self, item_level):
+        Item.__init__(self, item_level)
+        self.item_type = "Armor"
+        armor_list = ["leather", "brigandine", "plate_mail"]
+        self.armor_type = ""
+        self.armor_type = random.choice(armor_list)
+        self.defence = 1
+        if self.armor_type == "leather":
+            self.defence = self.item_level * 2
+        elif self.armor_type == "brigandine":
+            self.defence = self.item_level * 3
+        elif self.armor_type == "plate_mail":
+            self.defence = self.item_level * 4
+            
+         
+    def print_stats(self):
+        Item.print_stats(self)
+        print(self.armor_type + "_defence:" + str(self.defence) + "\n")
+        
