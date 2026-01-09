c9e06983f9d85589491a8dfb640e2cf7ff17d079
Zahara6639 <6639@student.sgdb.nl>
2026-01-09 21:29:54 +0100

    Add files via upload

---
+++ GAME/SaveGame.py
@@ -0,0 +1,37 @@
+import pickle
+import os
+
+SAVE_FILE = "savegame.dat"
+
+def save_game(player):
+    with open(SAVE_FILE, "wb") as file:
+        pickle.dump(player, file)
+    print("💾 Game saved!")
+
+def load_game():
+    if not os.path.exists(SAVE_FILE):
+        print("❌ No save game found.")
+        return None
+    
+    with open(SAVE_FILE, "rb") as file:
+        player = pickle.load(file)
+        
+    print("📂 Game loaded!")
+    return player
+
+def show_save_info():
+    if not os.path.exists(SAVE_FILE):
+        return False
+    
+    with open(SAVE_FILE, "rb") as file:
+        player = pickle.load(file)
+        
+        print("\n💾 Saved game found:")
+        
+    print("-" * 30)
+    print("Name :", player.name)
+    print("Level:", player.level)
+    print("HP   :", f"{player.hp} / {player.max_hp}")
+    print("Gold :", player.gold)
+    print("-" * 30)
+    return True
